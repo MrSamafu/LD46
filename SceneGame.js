28687929b43d64ae67f1dc9591dbc7221114c1db
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-21 17:50:38 +0200

    BugFix WASD

--- SceneGame.js
+++ SceneGame.js
@@ -189,15 +189,15 @@ class SceneGame extends Phaser.Scene{
     movePlayerManager(ship,target){
         // Creates object for input with WASD kets
         let moveKeys = this.input.keyboard.addKeys({
-            'up': Phaser.Input.Keyboard.KeyCodes.Z,
+            'up': Phaser.Input.Keyboard.KeyCodes.W,
             'down': Phaser.Input.Keyboard.KeyCodes.S,
-            'left': Phaser.Input.Keyboard.KeyCodes.Q,
+            'left': Phaser.Input.Keyboard.KeyCodes.A,
             'right': Phaser.Input.Keyboard.KeyCodes.D
         });
         // Rotates player to face towards reticle
         ship.rotation = Phaser.Math.Angle.Between(ship.x, ship.y, target.x, target.y)+Phaser.Math.DegToRad(90);
         // Enables movement of player with WASD keys
-        this.input.keyboard.on('keydown_Z', function (event) {
+        this.input.keyboard.on('keydown_W', function (event) {
             gameSettings.energy -= 2;
             ship.setAccelerationY(-gameSettings.playerSpeed);   
         });
@@ -205,7 +205,7 @@ class SceneGame extends Phaser.Scene{
             gameSettings.energy -= 2;
             ship.setAccelerationY(gameSettings.playerSpeed);  
         });
-        this.input.keyboard.on('keydown_Q', function (event) {
+        this.input.keyboard.on('keydown_A', function (event) {
             gameSettings.energy -= 2;
             ship.setAccelerationX(-gameSettings.playerSpeed);   
         });
@@ -214,7 +214,7 @@ class SceneGame extends Phaser.Scene{
             ship.setAccelerationX(gameSettings.playerSpeed);   
         });
         // Stops player acceleration on uppress of WASD keys
-        this.input.keyboard.on('keyup_Z', function (event) {
+        this.input.keyboard.on('keyup_W', function (event) {
             if (moveKeys['down'].isUp)
                 
                 ship.setAccelerationY(0);
@@ -224,7 +224,7 @@ class SceneGame extends Phaser.Scene{
                 
                 ship.setAccelerationY(0);
         });
-        this.input.keyboard.on('keyup_Q', function (event) {
+        this.input.keyboard.on('keyup_A', function (event) {
             if (moveKeys['right'].isUp)
                 
                 ship.setAccelerationX(0);
@@ -262,7 +262,7 @@ class SceneGame extends Phaser.Scene{
             game.input.mouse.requestPointerLock();
         });
 
-        // Exit pointer lock when A or escape (by default) is pressed.
+        // Exit pointer lock when Q or escape (by default) is pressed.
         this.input.keyboard.on('keydown_A', function (event) {
             if (game.input.mouse.locked)
                 game.input.mouse.releasePointerLock();
